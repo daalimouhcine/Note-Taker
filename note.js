674febab649d5296c611fb8a0ec8b18b1c0bd399
daalimouhcine <daalim277@gmail.com>
2020-11-09 23:48:41 +0100

    Add files via upload

--- note.js
+++ note.js
@@ -3,10 +3,12 @@ const submit = document.querySelector("#submit");
 const input = document.querySelector("#note");
 const notesContainer = document.querySelector(".notes-container");
 
+let NumNotes = 1;
 
 
-let notes = 1;
+
 //Creat Events
+document.addEventListener("DOMContentLoaded",setLocal);
 submit.addEventListener("click",addNote);
 
 
@@ -34,13 +36,13 @@ function addNote(e) {
         noteDiv.appendChild(noteButton);
         noteDiv.appendChild(close);
 
-        noteH3.innerText = "Note " + notes;
+        noteH3.innerText = "Note " + NumNotes;
         noteP.innerText = input.value;
         noteButton.innerText = "View Detail";
 
         saveLocal(input.value);
 
-        notes++;
+        NumNotes++;
 
         input.value = "";
         
@@ -87,8 +89,9 @@ function closeNote(e) {
     target.classList.add("fall");
     target.addEventListener("transitionend",function(){
         target.remove();
+        deleteLocal(target);
     });
-    notes--;
+    NumNotes--;
 }
 
 
@@ -103,4 +106,62 @@ function saveLocal(item) {
 
     notes.push(item);
     localStorage.setItem("notes",JSON.stringify(notes));
+}
+
+function setLocal() {
+    let notes;
+
+    if(localStorage.getItem("notes") === null) {
+        notes = [];
+    } else {
+        notes = JSON.parse(localStorage.getItem("notes"));
+    }
+
+    notes.forEach(function(item) {
+
+        const noteDiv = document.createElement("div");
+        const noteH3 = document.createElement("h3");
+        const noteP = document.createElement("p");
+        noteP.classList.add("par");
+        const noteButton = document.createElement("button");
+        noteButton.classList.add("detail");
+        const close = document.createElement("button");
+        close.innerHTML = '<i class="fas fa-times"></i>';
+        close.classList.add("close-note");
+
+        noteH3.innerText = "Note " + NumNotes;
+        noteP.innerText = item;
+        noteButton.innerText = "View Detail";
+
+        //Append elements
+        notesContainer.appendChild(noteDiv);
+        noteDiv.appendChild(noteH3);
+        noteDiv.appendChild(noteP);
+        noteDiv.appendChild(noteButton);
+        noteDiv.appendChild(close);
+        
+        NumNotes++;
+
+        noteButton.addEventListener("click",focusNote);
+        close.addEventListener("click",closeNote);
+    });
+}
+
+function deleteLocal(item) {
+    let notes;
+
+    if(localStorage.getItem("notes") === null) {
+        notes = [];
+    } else {
+        notes = JSON.parse(localStorage.getItem("notes"));
+    }
+
+    let deleteLocal = item.querySelector("p").innerHTML;
+    deleteLocal = deleteLocal.split('<br>');
+    deleteLocal = deleteLocal.join("\n");
+    console.log(deleteLocal);
+    if(notes.indexOf(deleteLocal) >= 0) {
+        notes.splice(notes.indexOf(deleteLocal),1);
+        localStorage.setItem("notes",JSON.stringify(notes));
+    }
 }
